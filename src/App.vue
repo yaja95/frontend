7f3a848ace85eebaa052c4b78be4021c32d5a157
Christopher Durham <cad97@cad97.com>
2017-10-16 19:30:24 -0400

    Add basic navigation along design

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,73 @@
 <template lang="pug">
-  v-app(toolbar)
+  v-app
+    v-navigation-drawer(app v-model="drawer" persistent enable-resize-watcher)
+      v-list(dense)
+        v-list-tile(to="/" exact @click="closeGroups")
+          v-list-tile-action: v-icon home
+          v-list-tile-content: v-list-tile-title Home
+        //- TODO: cancel click when already open?
+        v-list-group(v-for="(group, idx) in groups", v-model="group.active" :key="idx")
+          v-list-tile(slot="item" :to="group.to")
+            v-list-tile-action: v-icon {{ group.icon }}
+            v-list-tile-content: v-list-tile-title {{ group.title }}
+            v-list-tile-action: v-icon keyboard_arrow_down
+          v-list-tile(v-for="(path, idx) in group.paths" :key="idx" :to="path.to")
+            v-list-tile-content: v-list-tile-title {{ path.title }}
+            v-list-tile-action: v-icon {{ path.icon }}
     v-toolbar(app dark dense).primary
-      router-link(to="/" tag="v-toolbar-title") Furman Computing in Comunity
-      v-spacer
-      router-link(to="/students")
-        v-btn.accent Students #[v-icon.ml-2 school]
-      router-link(to="/faculty")
-        v-btn.accent Faculty #[v-icon.ml-2 fa-file-text]
-      router-link(to="/courses")
-        v-btn.accent Courses #[v-icon.ml-2 assessment]
-      router-link(to="/partners")
-        v-btn.accent Partners #[v-icon.ml-2 people]
+      v-toolbar-side-icon(@click.stop="toggleDrawer")
+      router-link(tag="v-toolbar-title" to="/") Furman Computing in Comunity
     main
-      v-container(fluid)
+      v-content: v-container(fluid)
         v-fade-transition(mode="out-in")
           router-view
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: true,
+      groups: [
+        {
+          to: '/students/',
+          icon: 'school',
+          title: 'Students',
+          active: this.$route.path.startsWith('/students/'),
+          paths: [
+            { to: '/students/projects/', title: 'Projects' },
+            { to: '/students/experiences/', title: 'Experiences' },
+            { to: '/students/courses/', title: 'Courses' },
+            { to: '/students/awards/', title: 'Awards' }
+          ]
+        },
+        {
+          to: '/faculty/',
+          icon: 'fa-file-text',
+          title: 'Faculty',
+          active: this.$route.path.startsWith('/faculty/'),
+          paths: [
+            { to: '/faculty/student_fellows/', title: 'Student Fellows' },
+            { to: '/faculty/teaching_fellows/', title: 'Teaching Fellows' },
+            { to: '/faculty/courses/', title: 'Courses' },
+            { to: '/faculty/projects/', title: 'Projects' }
+          ]
+        }
+        // { to: '/courses/', icon: 'assessment', title: 'Courses' },
+        // { to: '/partners/', icon: 'people', title: 'Partners' }
+      ]
+    }
+  },
+  methods: {
+    closeGroups () {
+      this.groups.forEach(group => {
+        group.active = false
+      })
+    },
+    toggleDrawer () {
+      this.drawer = !this.drawer
+    }
+  }
+}
+</script>
+

b26bcab600b6c1f0cd15453c04045c453b0966bc
Unknown <brandonp1551@gmail.com>
2017-10-27 13:17:51 -0400

    New homepage design
    Placed text on card instead of beneath it.

--- src/routes/home.vue
+++ src/routes/home.vue
@@ -2,50 +2,74 @@
   <v-layout row wrap>
     <v-flex xs12 md6>
       <v-card>
-        <v-card-media src="http://icons.iconarchive.com/icons/icons8/android/256/Users-Student-icon.png" height="256px">
+        <v-card-media
+          class="white--text"
+          height="256px"
+          src="http://icons.iconarchive.com/icons/icons8/android/256/Users-Student-icon.png"
+        >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">Students<br></span>
+                <span id="student_text" class="red--text">Meet computing students and learn about opportunities available to students through CinC</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
         </v-card-media>
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">Students</h3>
-            <div>See all the opportunities offered to Computing in the Communitys students and apply for positions</div>
-          </div>
-        </v-card-title>
       </v-card>
     </v-flex>
     <v-flex xs12 md6>
       <v-card>
-        <v-card-media src="http://icons.iconarchive.com/icons/icons8/android/256/Users-Student-icon.png" height="256px">
+        <v-card-media
+          class="white--text"
+          height="256px"
+          src="http://icons.iconarchive.com/icons/icons8/android/256/Users-Student-icon.png"
+        >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">Faculty<br></span>
+                <span id="faculty_text" class="red--text">Meet the great faculty who make CinC possible</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
         </v-card-media>
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">Faculty</h3>
-            <div>Meet the faculty who make Computing in the Community possible</div>
-          </div>
-        </v-card-title>
       </v-card>
     </v-flex>
     <v-flex xs12 md6>
       <v-card>
-        <v-card-media src="http://icons.iconarchive.com/icons/icons8/android/256/Users-Student-icon.png" height="256px">
+        <v-card-media
+          class="white--text"
+          height="256px"
+          src="http://icons.iconarchive.com/icons/icons8/android/256/Users-Student-icon.png"
+        >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">Courses<br></span>
+                <span id="courses_text" class="red--text">Learn about the great computing courses already on offer, or give input on new courses</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
         </v-card-media>
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">Courses</h3>
-            <div>Submit ideas for Computing in the Community courses, give feedback on courses, and see currently offered courses</div>
-          </div>
-        </v-card-title>
       </v-card>
     </v-flex>
     <v-flex xs12 md6>
       <v-card>
-        <v-card-media src="http://icons.iconarchive.com/icons/icons8/android/256/Users-Student-icon.png" height="256px">
+        <v-card-media
+          class="white--text"
+          height="256px"
+          src="http://icons.iconarchive.com/icons/icons8/android/256/Users-Student-icon.png"
+        >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">Partners<br></span>
+                <span id="partners_text" class="red--text">Learn about the partners who make the initiative possible, or become a partner</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
         </v-card-media>
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">Partners</h3>
-            <div>Partner with Computing in the Community</div>
-          </div>
-        </v-card-title>
       </v-card>
     </v-flex>
   </v-layout>

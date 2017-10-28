ce84a0d41b60c846129a07035f824592514a9ad4
Christopher Durham <cad97@cad97.com>
2017-10-28 01:07:56 -0400

    Make four-card layout reusable
    Also grab some free stock photo links for temp images

---
+++ src/components/RouteCards.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  v-layout(row wrap)
+    v-flex(v-for="(card, idx) in cards" :key="idx" xs12 md6)
+      v-card(:to="card.route" dark="dark")
+        v-card-media(:height="cardHeight" :src="card.img")
+          v-container(fill-height fluid): v-flex(xs12).white--text.darken-background
+            .headline.text-xs-center {{ card.title }}
+            .subheading.text-xs-center {{ card.text }}
+</template>
+
+<style lang="stylus" scoped>
+.darken-background
+  background: rgba(0, 0, 0, 0.4)
+</style>
+
+
+<script>
+export default {
+  props: {
+    cards: {
+      type: Array,
+      validator (value) {
+        if (value.length % 2 !== 0) return false
+        return value.every(card => card.img && card.route && card.title && card.text)
+      }
+    },
+    cardHeight: {
+      type: Number,
+      default: 300
+    },
+    dark: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>

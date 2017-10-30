66dc8714b619c8c4a9733b511a4b7deebba59a7c
Richard Rasheed <Richard.Rasheed@furman.edu>
2017-10-30 19:18:07 -0400

    Events and Opportunities on students page
    Added cards to accomodate events and opportunities feeds on the student page.

--- src/routes/StudentsPage.vue
+++ src/routes/StudentsPage.vue
@@ -1,5 +1,57 @@
 <template>
-  <RouteCards :cards="cards"></RouteCards>
+  <div>
+  <RouteCards :cards="cards" class="mb-2"></RouteCards>
+    <v-layout row>
+      <v-flex xs12 md6>
+        <v-card>
+          <v-spacer></v-spacer>
+          <v-toolbar class="primary" dark>
+            <v-toolbar-title> Events</v-toolbar-title>
+          </v-toolbar>
+          <v-list two-line>
+            <template v-for="(item, index) in items">
+              <v-list-tile avatar ripple :key="index">
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                  <v-list-tile-sub-title class="grey--text text--darken-4">{{ item.headline }}</v-list-tile-sub-title>
+                  <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+                <v-list-tile-action>
+                  <v-list-tile-action-text>{{ item.action }}</v-list-tile-action-text>
+                  <v-icon color="grey lighten-1">star_border</v-icon>
+                </v-list-tile-action>
+              </v-list-tile>
+              <v-divider v-if="index + 1 < items.length" :key="index"></v-divider>
+            </template>
+          </v-list>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 md6>
+        <v-card>
+          <v-spacer></v-spacer>
+          <v-toolbar class="primary" dark>
+            <v-toolbar-title> Opportunities</v-toolbar-title>
+          </v-toolbar>
+          <v-list two-line>
+            <template v-for="(item, index) in items">
+              <v-list-tile avatar ripple :key="index">
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                  <v-list-tile-sub-title class="grey--text text--darken-4">{{ item.headline }}</v-list-tile-sub-title>
+                  <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+                <v-list-tile-action>
+                  <v-list-tile-action-text>{{ item.action }}</v-list-tile-action-text>
+                  <v-icon color="grey lighten-1">star_border</v-icon>
+                </v-list-tile-action>
+              </v-list-tile>
+              <v-divider v-if="index + 1 < items.length" :key="index"></v-divider>
+            </template>
+          </v-list>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 <script>

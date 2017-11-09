2b24543707d93bce227e2014c82a4481c15d7225
Ajay Williams <ysmailliw@gmail.com>
2017-11-09 01:31:05 -0500

    Update FacultyPage.vue
    I positioned the upcoming events at the top, removed the opportunities card, and I added fluid containers to all three card sections . I also add a button that will link to the full calendar. Still need to figure out how to add whitespace between the four-card section.

--- src/routes/FacultyPage.vue
+++ src/routes/FacultyPage.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-  <RouteCards :cards="cards" class="mb-1"></RouteCards>
-    <v-layout row wrap>
-      <v-flex xs12 md6>
+      <v-container fluid>
+      <v-flex xs12 sm12 md12 lg12 xl12>
         <v-card>
           <v-toolbar class="primary" dark>
             <v-toolbar-title>Events</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn class="black--text">Calendar
+            <v-icon light right>date_range</v-icon>
+            </v-btn>
           </v-toolbar>
           <v-list two-line>
             <template v-for="(item, index) in events">
@@ -21,26 +24,10 @@
           </v-list>
         </v-card>
       </v-flex>
-      <v-flex xs12 md6>
-        <v-card>
-          <v-toolbar class="primary" dark>
-            <v-toolbar-title>Upcoming Opportunities</v-toolbar-title>
-          </v-toolbar>
-          <v-list two-line>
-            <template v-for="(item, index) in opportunities">
-              <v-list-tile ripple :key="index">
-                <v-list-tile-content>
-                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                  <v-list-tile-sub-title>{{ item.date.toLocaleDateString()}}</v-list-tile-sub-title>
-                  <v-list-tile-sub-title>{{ item.description }}</v-list-tile-sub-title>
-                </v-list-tile-content>
-              </v-list-tile>
-              <v-divider v-if="index + 1 < opportunities.length" :key="index"></v-divider>
-            </template>
-          </v-list>
-        </v-card>
-      </v-flex>
-    </v-layout>
+      </v-container>
+  <RouteCards :cards="cards" class="mb-1"></RouteCards>
+    <v-layout row wrap></v-layout>
+    
   </div>
 </template>
 
@@ -52,28 +39,28 @@ export default {
     return {
       cards: [
         {
-          img: 'https://images.pexels.com/photos/265076/pexels-photo-265076.jpeg',
-          route: '/faculty/student_fellows',
+          img: 'https://www.exceptionnotfound.net/content/images/2015/04/the-coder.jpg',
+          route: '/students/sfellows/',
           title: 'Student Fellows',
-          text: 'Learn what is expected of a student fellow and how to become one'
+          text: 'Learn what is expected of a student fellow and how to apply for a fellowship'
         },
         {
-          img: 'https://images.pexels.com/photos/159935/pexels-photo-159935.jpeg',
-          route: '/faculty/teaching_fellows',
+          img: 'https://www.bls.gov/ooh/images/15202.jpg',
+          route: '/students/experiences',
           title: 'Teaching Fellows',
           text: 'Learn how to become an instructor within the Computing in the Community initiative'
         },
         {
           img: 'https://images.pexels.com/photos/12064/pexels-photo-12064.jpeg',
-          route: '/faculty/courses/',
+          route: '/students/courses',
           title: 'Courses',
-          text: 'Learn about opportunities to teach a Computing in the Community course'
+          text: 'See the list of computer courses currently offered, able to be offered, and student-suggested courses'
         },
         {
-          img: 'https://images.pexels.com/photos/630835/pexels-photo-630835.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb',
-          route: '/faculty/projects',
-          title: 'Projects',
-          text: 'Suggest, collaborate on, or learn about projects'
+          img: 'https://www.aiche.org/sites/default/files/styles/aiche_content/public/images/overview_page/Awards%20Image.jpg?itok=zEFw_Eni',
+          route: '/students/awards',
+          title: 'Projects and Scholars',
+          text: 'See the projects and honors that our faculty have demonstrated and garnered!'
         }
       ],
       events: [ // Example stub
@@ -86,9 +73,10 @@ export default {
       ],
       opportunities: [ // Example stub
         {
-          title: 'Hack Night ',
-          description: 'Hack in the land of your dreams! I really don\'t have anything clever to say... ',
-          date: new Date(2017, 10, 6)
+          title: 'Study Away',
+          description: 'Study away in the land of your dreams! I really don\'t have anything clever to say... ' +
+            'Lorem Ipsum dolor amet. Have some pseudo-latin to soothe your pet. This should be a AABB rhyme but I\'m lazy.',
+          applyAt: 'Don\'t @ me'
         }
       ]
     }

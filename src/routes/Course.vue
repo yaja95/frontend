1e39be2e720465bfaa77682848b8137a5513fd91
Unknown <cokonya@furman.edu>
2017-11-14 13:10:56 -0500

    Course Page
    basic layout

---
+++ src/routes/Course.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+  <v-layout column>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-toolbar color="indigo" dark>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>CinC Courses</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>search</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-container fluid grid-list-md class="grey lighten-4">
+        <v-layout row wrap>
+          <v-flex
+            v-bind="{ [`xs${card.flex}`]: true }"
+            v-for="card in cards"
+            :key="card.title"
+          >
+            <v-card>
+              <v-card-media
+                :src="card.src"
+                height="200px"
+              >
+                <v-container fill-height fluid>
+                  <v-layout fill-height>
+                    <v-flex xs12 align-end flexbox>
+                      <span class="headline white--text" v-text="card.title"></span>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card-media>
+              <v-card-actions class="white">
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon>favorite</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>bookmark</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-flex>
+  </v-layout>
+  </div>
+</template>
